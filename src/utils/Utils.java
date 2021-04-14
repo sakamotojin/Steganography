049e15ba114b5c5ce658d89519c219d6be7bc371
Sumit Joshi <sumit151997@gmail.com>
2021-04-14 23:57:47 +0530

    Added Ui

---
+++ src/utils/Utils.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author Sakamoto Jin
+ */
+public class Utils {
+    
+    static boolean checkIfImageFile(String filePath){
+        
+        return true;
+    }
+    
+    static boolean checkIfValidFileSize(String filePath){
+        
+        return true;
+    }
+    
+    
+    
+}
